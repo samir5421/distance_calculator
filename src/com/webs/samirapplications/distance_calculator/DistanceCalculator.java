3146349b4a6128c1438fcde1797249d26d3d4a67
Samir <samir314159@gmail.com>
2010-08-30 19:38:27 -0500

    first commit

---
+++ src/com/webs/samirapplications/distance_calculator/DistanceCalculator.java
@@ -0,0 +1,87 @@
+package com.webs.samirapplications.distance_calculator;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+
+
+
+public class DistanceCalculator extends Activity {
+    /** Called when the activity is first created. */
+	
+	private EditText x1;
+	private EditText x2;
+	private EditText y1;
+	private EditText y2;
+	private double ans;
+	private TextView txtans;
+	private Button calculate;
+	private Button reset;
+	private double xOne;
+	private double yOne;
+	private double xTwo;
+	private double yTwo;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        initControls();
+    }
+    
+    private void initControls()
+    {
+    	calculate = (Button)findViewById(R.id.calculate);
+    	txtans = (TextView)findViewById(R.id.txtans);
+    	reset = (Button)findViewById(R.id.reset);
+    	x1 = (EditText)findViewById(R.id.x1);
+    	x2 = (EditText)findViewById(R.id.x2);
+    	y1 = (EditText)findViewById(R.id.y1);
+    	y2 = (EditText)findViewById(R.id.y2);
+    	
+    	calculate.setOnClickListener(new Button.OnClickListener()
+    	{
+    		public void onClick (View v)
+      		{
+    			calculate();
+      		} 
+      		{Toast.makeText(DistanceCalculator.this, R.string.short_notification_text, Toast.LENGTH_SHORT).show();}
+       	});
+    	
+    	reset.setOnClickListener(new Button.OnClickListener() 
+		{ public void onClick (View v)
+			{
+				reset(); 
+			}
+		});
+    	
+    }
+    public void calculate()
+    {
+    	xOne=Double.parseDouble(x1.getText().toString());
+    	xTwo=Double.parseDouble(x2.getText().toString());
+    	yOne=Double.parseDouble(y1.getText().toString());
+    	yTwo=Double.parseDouble(y2.getText().toString());
+		
+    	double x = ( xTwo - xOne ); 
+		double y = ( yTwo - yOne );
+		ans = Math.sqrt ( x*x + y*y );
+				
+		txtans.setText(Double.toString(ans));
+    }
+    
+    private void reset()
+	{
+		txtans.setText("");
+		x1.setText("");
+		x2.setText("");
+		y1.setText("");
+		y2.setText("");
+	}
+}
